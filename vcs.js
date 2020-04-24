0a0f83ba4c40564bad354e6c0804494729ed7e07
maxHartman <max@5hearts.net>
2020-04-24 12:38:48 -0400

    added some docstrings

--- vcs.js
+++ vcs.js
@@ -1,31 +1,43 @@
 const primitives = require('./cryptoPrimitives');
 const utils = require('./utils');
 
-/* 
- * Encode(x, P) = encrypt x with each of the public keys in P and output the corresponding vector of ciphertexts. Call this vector e.
+/**
+ * Encrypt a string with each public key.
+ * @param 	{String} 		plaintext	Plain text to be encrypted
+ * @param 	{Array<String>}	publicKeys	Vector of public keys
+ * @returns	{Array<String>}				Encoded vector
  */
 function encode(plaintext, publicKeys) {
 	var cipherVector;
 
 	for (const publicKey of publicKeys) {
-		cipherVector.push(primitives.encryptRsa(plaintext, publicKey))
+		cipherVector.push(primitives.encryptRsa(plaintext, publicKey));
 	}
 
 	return cipherVector;
 }
 
-/*
- * Decode(e, si, P) = use your secret key si to decrypt the i-th entry in e. This yields x.
+/**
+ * Decode a specific entry of a vector using the secret key.
+ * @param 	{Array<String>}	cipherVector	Encoded vector
+ * @param 	{Number} 		i				Index in question
+ * @param 	{String} 		secretKey		Decription key
+ * @returns	{String}						Plain text version of ith entry
  */
-function decode(cipherVector, i, secretKey, publicKeys) {
-	// TODO: consider removing publicKeys
+function decode(cipherVector, i, secretKey) {
 	const ciphertext = cipherVector[i];
 	const plaintext = primitives.decryptRsa(ciphertext, secretKey);
+
 	return plaintext;
 }
 
-/*
- * Verify(e, si, P) = verify that every entry in e encrypts the same value of x.
+/**
+ * Check that every entry in the vector encrypts the same value.
+ * @param 	{Array<String>}	cipherVector	Encoded vector
+ * @param 	{Number} 		i				Index in question
+ * @param 	{String} 		secretKey		Decription key
+ * @param 	{Array<String>}	publicKeys		Vector of public keys
+ * @returns {Boolean}						The validity of the comparison
  */
 function verify(cipherVector, i, secretKey, publicKeys) {
 	const plaintext = decode(cipherVector, i, secretKey, publicKeys);
